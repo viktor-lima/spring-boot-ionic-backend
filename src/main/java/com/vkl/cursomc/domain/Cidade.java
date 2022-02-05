67ada123bf63d308d43c384ef10aef7a1002422e
viktor-lima <joaolimak10@outlook.com>
2022-02-05 13:07:36 -0300

    Estado e Cidade

---
+++ src/main/java/com/vkl/cursomc/domain/Cidade.java
@@ -0,0 +1,78 @@
+package com.vkl.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Cidade implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "estado_id")
+	private Estado estado;
+
+	public Cidade() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Cidade(Integer id, String name, Estado estado) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.estado = estado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
