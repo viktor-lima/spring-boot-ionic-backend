f682b3566e62becb60a3a3a8f2141f7f98f1b1ed
viktor-lima <joaolimak10@outlook.com>
2022-02-05 21:55:19 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/vkl/cursomc/domain/Estado.java
+++ src/main/java/com/vkl/cursomc/domain/Estado.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Estado implements Serializable {
 
@@ -19,7 +21,8 @@ public class Estado implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
-
+	
+	@JsonBackReference
 	@OneToMany(mappedBy = "estado")
 	private List<Cidade> cidades = new ArrayList<>();
 
