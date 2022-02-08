d3b973b7e8d93f0a721e6dfa03c8406471112a3f
viktor-lima <joaolimak10@outlook.com>
2022-02-08 19:51:09 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/vkl/cursomc/domain/Cliente.java
+++ src/main/java/com/vkl/cursomc/domain/Cliente.java
@@ -16,7 +16,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.vkl.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -31,7 +30,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference
+	
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> encerecos = new ArrayList<>();
 	
