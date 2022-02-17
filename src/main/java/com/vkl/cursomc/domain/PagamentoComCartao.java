55264e2deaffc14f5fd7ffdd23de78e6220e64bd
viktor-lima <joaolimak10@outlook.com>
2022-02-17 14:30:40 -0300

    Inserindo Pedido

--- src/main/java/com/vkl/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/vkl/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.vkl.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.vkl.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/vkl/cursomc/services/PedidoService.java
+++ src/main/java/com/vkl/cursomc/services/PedidoService.java
@@ -1,23 +1,62 @@
 package com.vkl.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vkl.cursomc.domain.ItemPedido;
+import com.vkl.cursomc.domain.PagamentoComBoleto;
 import com.vkl.cursomc.domain.Pedido;
+import com.vkl.cursomc.domain.enums.EstadoPagamento;
+import com.vkl.cursomc.repositories.ItemPedidoRepository;
+import com.vkl.cursomc.repositories.PagamentoRepository;
 import com.vkl.cursomc.repositories.PedidoRepository;
+import com.vkl.cursomc.repositories.ProdutoRepository;
 import com.vkl.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
+	
+	@Autowired
+	private BoletoService boletoService; 
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto,obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido i : obj.getItens()) {
+			i.setDesconto(0.0);
+			i.setPrice(produtoRepository.findById(i.getProduto().getId()).get().getPrice());
+			i.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 
 }
