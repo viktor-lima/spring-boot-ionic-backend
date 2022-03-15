58d498aaff62caf7e0b6518763dcf06acdd8cb44
viktor-lima <joaolimak10@outlook.com>
2022-03-15 17:55:19 -0300

    Implementando autenticacao e geracao do tokem JWT

---
+++ src/main/java/com/vkl/cursomc/security/JWTAuthenticationFilter.java
@@ -0,0 +1,82 @@
+package com.vkl.cursomc.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vkl.cursomc.dto.CredenciaisDTO;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private AuthenticationManager authenticationManager;
+
+	private JWTUtil jwtUtil;
+
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+		setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
+		this.authenticationManager = authenticationManager;
+		this.jwtUtil = jwtUtil;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+
+		try {
+			CredenciaisDTO credenciais = new ObjectMapper().readValue(request.getInputStream(), CredenciaisDTO.class);
+
+			UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+					credenciais.getEmail(), credenciais.getSenha(), new ArrayList<>());
+
+			Authentication authentication = authenticationManager.authenticate(authenticationToken);
+			return authentication;
+
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+
+		String userName = ((UserSS) authResult.getPrincipal()).getUsername();
+		String tokem = jwtUtil.generateTokem(userName);
+		response.addHeader("Authorization", "Bearer " + tokem);
+	}
+
+	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
+		 
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+                throws IOException, ServletException {
+            response.setStatus(401);
+            response.setContentType("application/json"); 
+            response.getWriter().append(json());
+        }
+        
+        private String json() {
+            long date = new Date().getTime();
+            return "{\"timestamp\": " + date + ", "
+                + "\"status\": 401, "
+                + "\"error\": \"Não autorizado\", "
+                + "\"message\": \"Email ou senha inválidos\", "
+                + "\"path\": \"/login\"}";
+        }
+    }
+	
+	
+}
---
+++ src/main/java/com/vkl/cursomc/services/UserDatailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.vkl.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.vkl.cursomc.domain.Cliente;
+import com.vkl.cursomc.repositories.ClienteRepository;
+import com.vkl.cursomc.security.UserSS;
+
+@Service
+public class UserDatailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cliente = repo.findByEmail(email);
+		if(cliente == null)
+			throw new UsernameNotFoundException(email);
+		
+		return new UserSS(cliente.getId(), cliente.getEmail(), cliente.getSenha(), cliente.getPerfis());
+	}
+
+}
