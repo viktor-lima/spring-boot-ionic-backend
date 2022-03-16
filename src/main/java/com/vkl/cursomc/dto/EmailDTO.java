52a19d78455615f651fa17e07b11bccef8d9bf77
viktor-lima <joaolimak10@outlook.com>
2022-03-16 01:34:31 -0300

    Esqueci a senha

---
+++ src/main/java/com/vkl/cursomc/dto/EmailDTO.java
@@ -0,0 +1,31 @@
+package com.vkl.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class EmailDTO implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")
+	private String email;
+	
+	public EmailDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
--- src/main/java/com/vkl/cursomc/services/EmailService.java
+++ src/main/java/com/vkl/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.vkl.cursomc.domain.Cliente;
 import com.vkl.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -16,6 +17,6 @@ public interface EmailService {
 	
 	void sendHtmlEmail(MimeMessage msg);
 	
-	
+	void  sendNewPasswordEmail(Cliente cliente, String newPass);
 	
 }
