885c7e12d77fbe79397e61ae63d436bdd2d34a31
viktor-lima <joaolimak10@outlook.com>
2022-03-16 00:35:57 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/vkl/cursomc/services/PedidoService.java
+++ src/main/java/com/vkl/cursomc/services/PedidoService.java
@@ -4,18 +4,22 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.vkl.cursomc.domain.Cliente;
 import com.vkl.cursomc.domain.ItemPedido;
 import com.vkl.cursomc.domain.PagamentoComBoleto;
 import com.vkl.cursomc.domain.Pedido;
-import com.vkl.cursomc.domain.Produto;
 import com.vkl.cursomc.domain.enums.EstadoPagamento;
 import com.vkl.cursomc.repositories.ItemPedidoRepository;
 import com.vkl.cursomc.repositories.PagamentoRepository;
 import com.vkl.cursomc.repositories.PedidoRepository;
-import com.vkl.cursomc.repositories.ProdutoRepository;
+import com.vkl.cursomc.security.UserSS;
+import com.vkl.cursomc.services.exceptions.AuthorizationException;
 import com.vkl.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -71,6 +75,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		UserSS user = UserService.authenticated();
+		if(user == null) 
+			throw new AuthorizationException("Acesso negado!");
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
