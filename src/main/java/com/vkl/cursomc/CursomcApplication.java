44f81e43c374e33ed439b94b58e5f1438b606af5
viktor-lima <joaolimak10@outlook.com>
2022-03-26 18:38:10 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/vkl/cursomc/CursomcApplication.java
+++ src/main/java/com/vkl/cursomc/CursomcApplication.java
@@ -1,11 +1,17 @@
 package com.vkl.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vkl.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -14,6 +20,7 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		s3Service.uploadFile("C:\\Users\\joaol\\Documents\\java\\Projeto fullStack java-ionic\\img\\ana.jpg");
 		
 		
 	}
